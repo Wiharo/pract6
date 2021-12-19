511cf734b7995f216a18dc77f37c3ecc0730f34c
wiharo <grigory.lew4encko@yandex.ru>
2021-12-19 18:41:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {10, 2, 4, 5, 6, 7, 8};
+        Sorting.insertionSort(array);
+    }
+}
